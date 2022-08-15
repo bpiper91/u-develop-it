42d986dbe4f00acbd7839faf49ee26a0803ce0b3
bpiper91 <bpiper91@gmail.com>
2022-08-14 22:34:07 -0400

    added mysql connection to server.js

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const credentials = require('./config.js')
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+// Connect to database
+const db = mysql.createConnection(
+    {
+        'host': 'localhost',
+        // Your MySQL username,
+        'user': 'root',
+        // Your MySQL password
+        'password': credentials.mySQLpassword,
+        'database': 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
